7a8bad43a2d38d12d8f9da859b4c149fe0444097
Imiolek Daniel <imiolek.daniel.samuel@gmail.com>
2017-12-13 14:15:32 +0100

    Hello World

---
+++ app/src/androidTest/java/imiolekdaniel/pumlab2/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package imiolekdaniel.pumlab2;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("imiolekdaniel.pumlab2", appContext.getPackageName());
+    }
+}
