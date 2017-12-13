83aa9f3fd51cfae65195f204eac96806dfc3794e
Imiolek Daniel <imiolek.daniel.samuel@gmail.com>
2017-12-13 15:27:02 +0100

    Logika początek biedny

--- app/src/main/java/imiolekdaniel/pumlab2/MainActivity.java
+++ app/src/main/java/imiolekdaniel/pumlab2/MainActivity.java
@@ -2,6 +2,9 @@ package imiolekdaniel.pumlab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       Button Button_Miejsce =findViewById(R.id.Button1);
+        Button Lista =findViewById(R.id.Button2);
+        EditText EditText= findViewById((R.id.EditText));
+        TextView TextView= findViewById((R.id.TextView));
+
+
+        Button_Miejsce.setOnClickListener();
+
     }
 }
+
