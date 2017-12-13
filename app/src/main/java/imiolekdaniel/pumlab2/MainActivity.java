bca8a671fffda41e610603b17cf2e9450d7577d1
Imiolek Daniel <imiolek.daniel.samuel@gmail.com>
2017-12-13 20:20:48 +0100

    Podpiete widoki i obsługa przycisków

--- app/src/main/java/imiolekdaniel/pumlab2/MainActivity.java
+++ app/src/main/java/imiolekdaniel/pumlab2/MainActivity.java
@@ -1,7 +1,9 @@
 package imiolekdaniel.pumlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,13 +17,35 @@ public class MainActivity extends AppCompatActivity {
 
 
        Button Button_Miejsce =findViewById(R.id.Button1);
-        Button Lista =findViewById(R.id.Button2);
-        EditText EditText= findViewById((R.id.EditText));
-        TextView TextView= findViewById((R.id.TextView));
+        Button Button_Lista =findViewById(R.id.Button2);
+        final EditText EditText= findViewById((R.id.EditText));
+        final TextView TextView= findViewById((R.id.TextView));
 
 
-        Button_Miejsce.setOnClickListener();
+        //Button_Miejsce.setOnClickListener();
+
+        Button_Lista.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+               Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+               startActivity(intent);
+            }
+        });
+
+        Button_Miejsce.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+              String zmienna =EditText.getText().toString();
+               TextView.setText(zmienna);
+            }
+        });
+
+
+
 
     }
-}
+
+
+    }
+
 
